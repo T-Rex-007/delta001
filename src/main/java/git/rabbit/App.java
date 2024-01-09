706a6e73c7229091013e993b2cb8d979f0f9714c
sharunsher-007 <120362560+sharunsher-007@users.noreply.github.com>
2024-01-09 13:53:19 +0530

    new project file is added

---
+++ src/main/java/git/rabbit/App.java
@@ -0,0 +1,13 @@
+package git.rabbit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
