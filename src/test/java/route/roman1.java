73248096678d37f6c8de2c008a6ca83e32dbebe2
sharunsher-007 <120362560+sharunsher-007@users.noreply.github.com>
2024-01-10 21:23:33 +0530

    add another method

--- src/test/java/route/roman1.java
+++ src/test/java/route/roman1.java
@@ -5,5 +5,11 @@ public class roman1 {
 	public void r1() {
 		
 		System.out.println("hi world");
+		
+		public void r2() {
+			
+			System.out.println("hi world");
+			
+			System.out.println("r1");
 	}
 }
