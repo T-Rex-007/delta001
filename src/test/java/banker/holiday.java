671699391f982b8db6ac82a90e24ddc166a3b1f2
haritha14hari <144885259+haritha14hari@users.noreply.github.com>
2024-01-26 12:06:07 +0530

    added loop statement

--- src/test/java/banker/holiday.java
+++ src/test/java/banker/holiday.java
@@ -1,10 +1,45 @@
 package banker;
 
+import java.util.Scanner;
+
 public class holiday {
 	
 	public void  method() {
-		System.out.println("good morning");
+		
+		  Scanner scanner = new Scanner(System.in);
+
+	        // Prompt the user to enter a number
+	        System.out.println("Enter a number (or any non-numeric value to exit):");
+
+	        // Loop to calculate the square of user-entered numbers
+	        while (true) {
+	            try {
+	                // Read the input as a string
+	                String input = scanner.nextLine();
+
+	                // Attempt to convert the input to a double
+	                double num = Double.parseDouble(input);
+
+	                // Calculate the square of the number and print it
+	                double square = num * num;
+	                System.out.println("The square of " + num + " is " + square);
+
+	            } catch (NumberFormatException e) {
+	                // If the input cannot be converted to a number, exit the loop
+	                System.out.println("Exiting the program.");
+	                break;
+	            }
+	        }
+
+	        // Close the scanner
+	        scanner.close();
+	    }
+
+		
+		
+		
+		
 	}
 	
 
-}
+
